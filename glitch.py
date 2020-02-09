54c693512adae6bb45e6f1f23b1f4894eaea7628
djbroccoli <59752772+djbroccoli@users.noreply.github.com>
2020-02-09 11:42:25 -0800

    clened up code

--- glitch.py
+++ glitch.py
@@ -9,7 +9,7 @@ song = song[40000:80000] # take a section of that audio (in milliseconds if you 
 
 song_length = len(song) # length of song in milliseconds
 
-# song_iterator = iter(list(range(song_length))) #creates a list with all values from song to be stepped through with .__next__
+
 
 def repeater(clip, n):  # repeats audio clip n times
     return clip * n
@@ -19,20 +19,6 @@ def slicer(audioClip, beginning, end):  # takes a begging number and an end mili
 
 def gluer(audio1, audio2): #appends together sliced audio
     return audio1 + audio2
-
-def x_second_looper(x, secondChunks): #takes x, how many times to repeat each chunk, and secondChunk, the size of the chunk in milliseconds
-    t = song_length // (secondChunks)
-    newClip = AudioSegment.empty()
-    for n in range(t):
-        if n == 0:
-            n += 1
-        else:
-            ticker = n * (secondChunks)
-            buffer = repeater(slicer(song, (ticker-(secondChunks)), ticker), x)
-            newClip = newClip + buffer
-    newClip.export("loopedMiley.mp3", format="mp3")
-
-# x_second_looper(3, 50)
 
 def glitch(): # a random glitch machine
 
@@ -51,35 +37,6 @@ def glitch(): # a random glitch machine
 
 glitch()
 
-
-#newClip = AudioSegment.empty()
-# def two_seconds(place): #repeats every 2 seconds of song, init with 0
-#     i = (place)
-#
-#     global newClip
-#     if (i == 2000): # this is for the first pass through, grabs the first two seconds and adds to new clip, makes recursive call
-#         firstAudioClip = repeater(slicer((i - 2000), i), 2)
-#         newClip = firstAudioClip
-#         n = next(song_iterator)
-#         two_seconds(n)
-#     elif i == 0:
-#         next(song_iterator)
-#     elif (i % 2000) == 0:
-#         floatingAudio = repeater(slicer((i - 2000), i), 2)
-#         newClip = newClip + floatingAudio
-#         n = next(song_iterator)
-#         two_seconds(n)
-#     else:
-#         n = next(song_iterator)
-#         if n == StopIteration:
-#             print("Done")
-#             newClip.export("two_seconds.mp3", type="mp3")
-#             return None
-#         else:
-#             two_seconds(n)
-#
-#
-# two_seconds(1)
 
 
 
